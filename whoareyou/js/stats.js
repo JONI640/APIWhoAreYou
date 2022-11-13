0f173b54b8f4ff2c14fc0390a2e178e55edd3471
Jon Ander <jonander2001@gmail.com>
2022-11-13 16:00:51 +0100

    milestone4

--- whoareyou/js/stats.js
+++ whoareyou/js/stats.js
@@ -3,11 +3,10 @@ export {initState}
 let initState = function(what, solutionId) { 
     // Vemos si el objeto que queremos esta en el localStorage
     // Si esta, añadimos el nuevo intento y actualizamos
-    let v = localStorage.getItem(what)
-    console.log(v)
-    if(localStorage.getItem(what) != null){      
-        return [localStorage.getItem(what), function(guess) {
-            let nuevo = JSON.parse(localStorage.getItem(what))
+    let aux = localStorage.getItem(what)
+    if(aux != null){      
+        return [aux, function(guess) {
+            let nuevo = JSON.parse(aux)
             console.log(nuevo)
             nuevo["guesses"].push(guess)
             nuevo["solution"] = solutionId
@@ -16,7 +15,7 @@ let initState = function(what, solutionId) {
     }
     // Si no esta, lo creamos y lo añadimos
     else{
-        return [localStorage.getItem(what), function(guess) {
+        return [aux, function(guess) {
             let situacion = { "guesses" : [], "solution": solutionId}
             situacion["guesses"].push(guess)
             localStorage.setItem("WAYgameState", situacion)
