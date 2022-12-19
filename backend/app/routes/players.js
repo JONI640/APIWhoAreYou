ee7495c1f1ef8646d88c1a77950306fe32fe85e4
asierinsa <79907890+asierinsa@users.noreply.github.com>
2022-12-19 03:23:38 +0100

    Ejercicio 2.4
    Falta completar el formulario entero, solo he puesto el nombre

--- backend/app/routes/players.js
+++ backend/app/routes/players.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+//const { body, validationResult } = require('express-validator');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Ejercicio 2.4
+router.get('/add', (req, res) => {
+  res.render('add')
+})
+
+router.post('/add', function(req,res){
+  console.log(req.body)
+  res.render('add')
+})
 
 module.exports = router;
