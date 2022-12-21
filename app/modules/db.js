89e4c4e1229e829523f3b3a8a3f314665102215e
Adrian <adrian.molina115@gmail.com>
2022-12-21 20:52:45 +0100

    Ejercicio 5.1.1

---
+++ app/modules/db.js
@@ -0,0 +1,53 @@
+const mongojs = require('mongojs');
+
+const database = 'sw_db';
+const collection = 'players';
+
+const db = mongojs(`mongodb://127.0.0.1:27017/${database}`, [collection]);
+
+const getPlayerById = (id) => {
+    return new Promise((resolve, reject) => {
+        db[collection].findOne({id}, (err, doc) => {
+            if(err) reject(err);
+            else resolve(doc);
+        });
+    });
+}
+
+const removePlayer = (id) => {
+    db[collection].remove({id});
+}
+
+const insertPlayer = (params) => {
+    db[collection].insertOne(params);
+}
+
+const modifyPlayer = (params) => {
+    return new Promise((resolve, reject) => {
+        db[collection].findAndModify({
+            query: {id: params.id},
+            update: {
+                id: params.id,
+                name: params.name,
+                birthdate: params.birthdate,
+                nationality: params.nationality,
+                teamId: params.teamId,
+                position: params.position,
+                number: params.number
+            }
+        }, (err, docs) => {
+            if(err) reject(err);
+            else resolve(docs)
+        });
+    })
+}
+
+
+const DB = {
+    getPlayerById,
+    removePlayer,
+    insertPlayer,
+    modifyPlayer,
+}
+
+module.exports = DB;
--- app/routes/players.js
+++ app/routes/players.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
+const DB = require('../modules/db');
 
 //const { body, validationResult } = require('express-validator');
 
@@ -15,25 +16,56 @@ router.get('/add', (req, res) => {
 })
 
 router.post('/add',
-    [check('name').not().isEmpty().withMessage('Se requiere un nombre'),
-        check('id').not().isEmpty().withMessage('Introducce un id'),
-        check('birth').not().isEmpty().withMessage('Introducce una fecha de nacimiento'),
-        check('nation').not().isEmpty().withMessage('Introducce a que pais pertenece'),
-        check('team').not().isEmpty().withMessage('Introducce a que equipo pertenece'),
-        check('position').not().isEmpty().withMessage('Introducce en que posición juega'),
-        check('number').not().isEmpty().withMessage('Introducce el número del jugador')],
-    function(req,res){
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-       // res.locals.message = "Se han cometido errores" //err.message;7
-          // res.locals.error = req.app.get('env') === 'development' ? 400 : {};
-        console.log(errors.errors)
-        res.render('add_error', { title: 'Error', mensaje: errors.errors[0].msg })
-      }
-      else {
-        console.log(req.body)
-        res.render('add')
-      }
+  [check('name').not().isEmpty().withMessage('Se requiere un nombre'),
+  check('id').not().isEmpty().withMessage('Introducce un id'),
+  check('birth').not().isEmpty().withMessage('Introducce una fecha de nacimiento'),
+  check('nation').not().isEmpty().withMessage('Introducce a que pais pertenece'),
+  check('team').not().isEmpty().withMessage('Introducce a que equipo pertenece'),
+  check('position').not().isEmpty().withMessage('Introducce en que posición juega'),
+  check('number').not().isEmpty().withMessage('Introducce el número del jugador')],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) res.render('add_error', { title: 'Error', mensaje: errors.errors[0].msg })
+
+    console.log(req.body)
+    const params = {
+      id: parseInt(req.body.id),
+      name: req.body.name,
+      birthdate : req.body.birth,
+      nationality: req.body.nation,
+      teamId: parseInt(req.body.team),
+      position: req.body.position,
+      number: parseInt(req.body.number)
+    }
+    DB.insertPlayer(params);
+    res.render('add')
+})
+
+router.get('/:id', async (req, res) => {
+  const aux = await DB.getPlayerById(parseInt(req.params.id));
+  console.log(aux);
+  res.json(aux);
+});
+
+router.get('/remove/:id', async (req, res) => {
+  DB.removePlayer(parseInt(req.params.id));
+  res.json({ok: true});
+})
+
+router.put('/edit', async (req, res) => {
+  const params = {
+    id: parseInt(req.body.id),
+    name: req.body.name,
+    birthdate: req.body.birth,
+    nationality: req.body.nation,
+    teamId: parseInt(req.body.team),
+    position: req.body.position,
+    number: parseInt(req.body.number)
+  }
+
+  const result = await DB.modifyPlayer(params);
+
+  res.json({ok:true, result});
 })
 
 module.exports = router;
