215838a1438dfbe91572836428f62e06e92decc8
JONI640 <71083468+JONI640@users.noreply.github.com>
2022-12-21 20:27:10 +0100

    Ejercicio 4.2
    Para cada uno de los comandos del bash almacenar el json resultante en la base de datos

---
+++ Ejercicios/Ejercicios 4/ejercicio4_2.bash
@@ -0,0 +1,5 @@
+curl --request GET --url "https://v3.football.api-sports.io/leagues?id=140" --header 'x-rapidapi-host: v3.football.api-sports.io' --header 'x-rapidapi-key: 73a6fce586222fe2e731f55c0b63f1d9'
+curl --request GET --url "https://v3.football.api-sports.io/leagues?id=39" --header 'x-rapidapi-host: v3.football.api-sports.io' --header 'x-rapidapi-key: 73a6fce586222fe2e731f55c0b63f1d9'
+curl --request GET --url "https://v3.football.api-sports.io/leagues?id=135" --header 'x-rapidapi-host: v3.football.api-sports.io' --header 'x-rapidapi-key: 73a6fce586222fe2e731f55c0b63f1d9'
+curl --request GET --url "https://v3.football.api-sports.io/leagues?id=61" --header 'x-rapidapi-host: v3.football.api-sports.io' --header 'x-rapidapi-key: 73a6fce586222fe2e731f55c0b63f1d9'
+curl --request GET --url "https://v3.football.api-sports.io/leagues?id=78" --header 'x-rapidapi-host: v3.football.api-sports.io' --header 'x-rapidapi-key: 73a6fce586222fe2e731f55c0b63f1d9'
