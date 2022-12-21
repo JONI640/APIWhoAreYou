4f6d3b2dae28afecb5f384b2b3083ba43a9973bd
JONI640 <71083468+JONI640@users.noreply.github.com>
2022-12-21 19:24:54 +0100

    Update app.js

--- Ejercicios/Ejercicios 4/app.js
+++ Ejercicios/Ejercicios 4/app.js
@@ -10,28 +10,27 @@ async function main(data) {
 
     // Itera sobre los identificadores de los equipos
     for (const elem of data) {
-        // Ejecuta la llamada a la API después de un tiempo determinado
-        setTimeout(async () => {
-            // Hace una llamada a la API para obtener los datos del equipo
-            console.log(elem)
-            const response = await fetch(`https://v3.football.api-sports.io/teams?id=${elem.newId}`, {
-                headers: {
-                    'X-RapidAPI-Key': '73a6fce586222fe2e731f55c0b63f1d9',
-                    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-                }
-            });
+        // Hace una llamada a la API para obtener los datos del equipo
+        console.log(elem)
 
-            const teamData = await response.json();
+        const response = await fetch(`https://v3.football.api-sports.io/teams?id=${elem.newId}`, {
+            headers: {
+                'X-RapidAPI-Key': '73a6fce586222fe2e731f55c0b63f1d9',
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
+        });
 
-            // Inserta los datos del equipo en la colección "teams" de MongoDB
-            teamsCollection.insert(teamData, (err, result) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(result);
-                }
-            });
-        }, 60000 / 10); // Ejecuta la llamada cada 60000 / 10 milisegundos = cada 6 segundos
+        const teamData = await response.json();
+
+        // Inserta los datos del equipo en la colección "teams" de MongoDB
+        teamsCollection.insert(teamData, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        });
+        await new Promise(resolve => setTimeout(resolve, 60000 / 10)); // Espera 6 segundos (60000 milisegundos)
     }
     // Cierra la conexión a la base de datos
     db.close();
